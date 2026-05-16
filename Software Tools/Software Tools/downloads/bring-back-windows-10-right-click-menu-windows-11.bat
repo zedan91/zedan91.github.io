edc5852d0f658dc15825d6d6b14d941b24ed4379
zedan0791 <zedan9107@gmail.com>
2026-05-17 00:23:40 +0800

    update

--- Software Tools/Software Tools/downloads/bring-back-windows-10-right-click-menu-windows-11.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('Selesai. Sila restart PC untuk effect Windows 10 right-click menu.', 'AZOBSS Windows Tweaks', 'OK', 'Information')"
-echo Selesai. Sila restart PC untuk effect.
-pause
